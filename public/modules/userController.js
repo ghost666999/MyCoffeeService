f41babe4610216e81d8e96ed96d5353dda808d54
Wang, Yang <yang.wang@news.com.au>
2016-11-25 11:45:24 +1100

    remove jquery code.

--- public/modules/userController.js
+++ public/modules/userController.js
@@ -12,6 +12,10 @@ userControllers.controller('userController', [ '$scope', '$location',  '$http', 
 	$scope.maxChartHeight = 0;
 	
 	$scope.selectedChartType = "days";
+
+	$scope.promotion = {};
+
+	$scope.promotions = [];
 	
 	$scope.getBarHeight = function (noOfUsers) {
 		var height = noOfUsers/$scope.maxNoOfUsers * 95;
@@ -40,6 +44,23 @@ userControllers.controller('userController', [ '$scope', '$location',  '$http', 
 
         self.getMaxNo(chartData);
 		$scope.userResponse.chartData = chartData;
+	}
+
+	$scope.addPromotions = function () {
+		$scope.promotions.push({
+			"header": $scope.promotion.header,
+			"desc": $scope.promotion.desc,
+			"className": "collapse"
+		})
+	}
+
+	$scope.showPromotion = function (index) {
+		var className = $scope.promotions[index].className === "active" ? "collapse" : "active";
+		$scope.promotions[index].className = className;
+	}
+
+	$scope.removePromotion = function (index) {
+		$scope.promotions.splice(index, 1);
 	}
 	
 	
